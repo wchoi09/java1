a5d918aa50b272b02626c961a45a48c9669ecb7e
wchoi09 <54845449+wchoi09@users.noreply.github.com>
2021-04-02 22:05:02 +0900

    Fifth commit

---
+++ 8_Variable/src/Letter.java
@@ -0,0 +1,9 @@
+public class Letter {
+    public static void main(String[] args) {
+        String name = "r0-dt";
+        System.out.println("Hello, "+name+" ... "+name+" ... r0-dt ... bye");
+        
+        double VAT = 10.0;
+        System.out.println(VAT);
+    }
+}
