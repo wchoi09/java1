38982667da2415e9dc7927e1c9779f8102a26b22
wchoi09 <54845449+wchoi09@users.noreply.github.com>
2021-04-04 02:12:02 +0900

    Ninth commit - instance, inheritance

---
+++ 13_ClassApp/src/App.java
@@ -0,0 +1,10 @@
+import java.io.FileNotFoundException;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+        System.out.println(Math.floor(1.6));
+        System.out.println(Math.ceil(1.6));
+
+    }
+}
